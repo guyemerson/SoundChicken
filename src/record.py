92a370fa50b4856b0e54d671c29a4fec79d58952
bigstas <bigstas_lives@hotmail.com>
2014-08-06 12:44:06 +0100

    Recording modules
    record - actually works
    detectrecord - experimental. Intended to detect silence and only record
    when there is sound. Currently not working.

---
+++ src/record.py
@@ -0,0 +1,47 @@
+"""
+PyAudio example: Record a few seconds of audio and save to a WAVE
+file.
+"""
+
+import pyaudio
+import wave
+import sys
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+
+if sys.platform == 'darwin':
+    CHANNELS = 1
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+filename = raw_input("Filename: ")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(filename + '.wav', 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
